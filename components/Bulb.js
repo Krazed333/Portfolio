3525e7d04fe7f56a9330b9f64ce6f91081948dee
Kamal <kkayramos3@gatech.edu>
2024-03-10 15:40:13 -0400

    Update Bulb component's style zIndex

--- components/Bulb.js
+++ components/Bulb.js
@@ -4,7 +4,7 @@ const Bulb = () => {
   return (
     <div
       className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]"
-      style={{ zIndex: 1 }}
+      style={{ zIndex: 0 }}
     >
       <Image
         src={"/bulb.png"}

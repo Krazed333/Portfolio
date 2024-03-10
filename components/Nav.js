4fb737eb05616d0b3aff255a89c62042599f7ed1
Kamal <kkayramos3@gatech.edu>
2024-03-10 14:27:54 -0400

    Fix styling issues in Nav and Home components

--- components/Nav.js
+++ components/Nav.js
@@ -35,7 +35,10 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+    <nav
+      className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen"
+      style={{ zIndex: 1 }}
+    >
       {/* inner container */}
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-2 md:px-10 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {

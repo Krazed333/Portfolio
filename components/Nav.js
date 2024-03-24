154c6f65409eb43f1ab959d30bb9b5105df1c9af
Kamal <kkayramos3@gatech.edu>
2024-03-24 18:08:33 -0400

    Add external link to Resume page

--- components/Nav.js
+++ components/Nav.js
@@ -15,8 +15,11 @@ export const navData = [
   { name: "Projects", path: "/Projects", icon: <HiViewColumns /> },
   {
     name: "Resume",
-    path: "/Resume",
+    path: "https://drive.google.com/file/d/13o0fcCS940S_XRhc5zKdPIdgI98Mj1Kd/view?usp=sharing",
     icon: <HiChatBubbleBottomCenterText />,
+    isExternal: true, // Add a flag to indicate this is an external link
+    target: "_blank", // Add a target to open the link in a new tab
+    rel: "noopener noreferrer", // Add rel for security reasons when using target="_blank"
   },
   {
     name: "contact",
@@ -42,7 +45,30 @@ const Nav = () => {
       {/* inner container */}
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-2 md:px-10 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {
-          return (
+          return link.isExternal ? (
+            <a
+              className={`${
+                link.path === pathname && "text-accent"
+              } relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={link.path}
+              key={index}
+              target={link.target}
+              rel={link.rel}
+            >
+              {/* tooltip */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px] rounded -[3px]">
+                  <div className="text-[12px] leading-none font-semibold capitalize">
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className="border-solid border-l-white border-l- border-y-trasparent border-y-[6px] border-r-white border-r- w-0 h-0"></div>
+                </div>
+              </div>
+              {/* icon */}
+              <div>{link.icon}</div>
+            </a>
+          ) : (
             <Link
               className={`${
                 link.path === pathname && "text-accent"
